7f6955ea4f729908e332a999ae4862f40ebafa32
MrAzimzada <mehemmedezimzase@gmail.com>
2023-02-08 13:21:47 +0400

    add admin panel testomonia

--- cycle/Areas/Admin/Controllers/TestomoniaController.cs
+++ cycle/Areas/Admin/Controllers/TestomoniaController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using cycle.Data;
+using cycle.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -14,16 +15,18 @@ namespace cycle.Areas.Admin.Controllers
     {
         private readonly ILogger<TestomoniaController> _logger;
         private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
 
-        public TestomoniaController(ILogger<TestomoniaController> logger, AppDbContext context)
+        public TestomoniaController(ILogger<TestomoniaController> logger, AppDbContext context, IWebHostEnvironment env)
         {
             _logger = logger;
             _context = context;
+            _env = env;
         }
 
         public IActionResult Index()
         {
-            var testomonia = _context.Testomonias.OrderByDescending(x=>x.Id).ToList();
+            var testomonia = _context.Testomonias.OrderByDescending(x => x.Id).ToList();
             return View(testomonia);
         }
 
@@ -32,8 +35,21 @@ namespace cycle.Areas.Admin.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Create(string content , string name , IFormFile photo)
+        public IActionResult Create(string content, string name, IFormFile photo)
         {
+
+            var path = "/uploads/" + Guid.NewGuid() + photo.FileName;
+            using (var fileStream = new FileStream(_env.WebRootPath + path, FileMode.Create))
+            {
+                photo.CopyTo(fileStream);
+            }
+
+            Testomonia testomonia = new Testomonia();
+            testomonia.content = content;
+            testomonia.name = name;
+            testomonia.PhotoUrl =path;
+            _context.Testomonias.Add(testomonia);
+            _context.SaveChanges();
             return RedirectToAction(nameof(Index));
         }
 
