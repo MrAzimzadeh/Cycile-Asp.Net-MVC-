476c82d29c15358135e80b90bd2b22ac4517b918
MrAzimzada <mehemmedezimzase@gmail.com>
2023-02-15 12:46:23 +0400

    Finishh Newss Update

--- cycle/Areas/Admin/Controllers/NewController.cs
+++ cycle/Areas/Admin/Controllers/NewController.cs
@@ -72,9 +72,41 @@ namespace cycle.Areas.Admin.Controllers
         public IActionResult Delete(News news)
         {
             var delete = _context.News.FirstOrDefault(x => x.Id == news.Id);
-            _context.News.Remove(delete);   
+            _context.News.Remove(delete);
             _context.SaveChanges();
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Update(int Id)
+        {
+            var update = _context.News.FirstOrDefault(x => x.Id == Id);
+            if (update.Id == null)
+            {
+                return RedirectToAction(nameof(Error));
+            }
+            return View(update);
+        }
+        [HttpPost]
+        public IActionResult Update(News m, IFormFile photo)
+        {
+            var news = _context.News.FirstOrDefault(x => x.Id == m.Id);
+            if (news == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            if (photo != null)
+            {
+                news.PhotoUrl = ImageHelper.UploadSinglePhoto(photo, _env);
+            }
+
+            news.Title = m.Title;
+            news.Content = m.Content;
+            news.PublishDate = DateTime.Now;
+            news.UserName = m.UserName;
+            _context.News.Update(news);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
